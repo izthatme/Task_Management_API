7e5b3f2bdfaac23796c4373c7f260c17500b6020
izthatme <pkr9532@gmail.com>
2025-09-05 15:47:15 +0530

    Updated frontend  with latest changes

--- frontend/tasker/src/api.js
+++ frontend/tasker/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export const api = axios.create({
-baseURL: "http://localhost:4000/api",
+baseURL: "https://task-management-api-lwre.onrender.com/api",
 withCredentials: true,
 });
